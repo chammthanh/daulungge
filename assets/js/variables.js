58c2061e26d6a45d3bb1949da82bbf4f251977ae
chammthanh <nguyenthithanhtram311@gmail.com>
2024-02-01 23:02:02 +0700

    add prd

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -21,107 +21,178 @@ var criterias = [{
 }];
 
 var products = [{
-    name: 'Doll nón thỏ đen',
-    price: '80.000',
-    photo: 'assets\/images\/products\/doll-non-tho-den-80.png',
-}, {
-    name: 'Logo Độ Mixi',
-    price: '65.000',
-    photo: 'assets\/images\/products\/do-mixi-65.png',
-}, {
-    name: 'Gà mập',
-    price: '55.000',
-    photo: 'assets\/images\/products\/ga-map-55.png',
-}, {
-    name: 'Hành lá',
-    price: '30.000',
-    photo: 'assets\/images\/products\/hanh-la-30.png',
-}, {
-    name: 'Mochi donut',
-    price: '35.000',
-    photo: 'assets\/images\/products\/mochi-donut-35.png',
-}, {
-    name: 'Mèo ú',
-    price: '35.000',
-    photo: 'assets\/images\/products\/meo-u-35.png',
-}, {
-    name: 'Núi & hoa',
-    price: '28.000',
-    photo: 'assets\/images\/products\/nui-hoa-28.png',
-}, {
-    name: 'Stitch',
-    price: '65.000',
-    photo: 'assets\/images\/products\/stitch-65.png',
-}, {
-    name: 'Vịt tỉu thơ',
-    price: '50.000',
-    photo: 'assets\/images\/products\/vit-tiu-tho-50.png',
-}, {
-    name: 'Vịt nón bèo',
-    price: '55.000',
-    photo: 'assets\/images\/products\/vit-non-beo-55.png',
-}, {
-    name: 'Vịt nâm xôi',
-    price: '60.000',
-    photo: 'assets\/images\/products\/vit-mam-xoi-60.png',
-}, {
-    name: 'Tulip',
-    price: '30.000',
-    photo: 'assets\/images\/products\/tulip-30.png',
-}, {
-    name: 'Bạch tuộc tròn',
-    price: '40.000',
-    photo: 'assets\/images\/products\/bach-tuoc-tron-40.png',
-}, {
-    name: 'Bồ hóng',
-    price: '45.000',
-    photo: 'assets\/images\/products\/bo-hong45.png',
-}, {
-    name: 'Bánh kem cuộn',
-    price: '28.000',
-    photo: 'assets\/images\/products\/banh-kem-cuon-28.png',
-}, {
-    name: 'Calcifer',
-    price: '28.000',
-    photo: 'assets\/images\/products\/calcifer-28.png',
-}, {
-    name: 'Cà chua',
-    price: '30.000',
-    photo: 'assets\/images\/products\/ca-chua-30.png',
-}, {
-    name: 'Bò sữa',
-    price: '50.000',
-    photo: 'assets\/images\/products\/bo-sua50.png',
-}, {
-    name: 'Chó shiba',
-    price: '50.000',
-    photo: 'assets\/images\/products\/cho-shiba-50.png',
-}, {
-    name: 'Con sứa',
-    price: '55.000',
-    photo: 'assets\/images\/products\/con-sua-55.png',
-}, {
-    name: 'Đầu loopy',
-    price: '50.000',
-    photo: 'assets\/images\/products\/dau-loopy-50.png',
-}, {
-    name: 'Mochi bánh giò',
-    price: '60.000',
-    photo: 'assets\/images\/products\/mochi-banh-gio-60.png',
-}, {
-    name: 'Ong vàng',
-    price: '45.000',
-    photo: 'assets\/images\/products\/ong-45.png',
-}, {
-    name: 'Đầu heo hồng',
-    price: '40.000',
-    photo: 'assets\/images\/products\/dau-heo-hong-40.png',
-}, {
-    name: 'Minions',
-    price: '45.000',
-    photo: 'assets\/images\/products\/minions-45.png',
-}, {
-    name: 'Đồ ăn',
-    price: '50.000',
-    photo: 'assets\/images\/products\/do-an-50.png',
-}];
+        name: 'Nấm mini (nhiều mẫu khác)',
+        price: '30.000',
+        photo: 'assets\/images\/products\/nam-mini-30.jpg',
+    }, {
+        name: 'Doll nón thỏ đen',
+        price: '85.000',
+        photo: 'assets\/images\/products\/doll-non-tho-den-80.png',
+    }, {
+        name: 'Logo Độ Mixi',
+        price: '65.000',
+        photo: 'assets\/images\/products\/do-mixi-65.png',
+    }, {
+        name: 'Gà mập',
+        price: '60.000',
+        photo: 'assets\/images\/products\/ga-map-60.png',
+    }, {
+        name: 'Lightsick Sơn Tùng MTP',
+        price: '55.000',
+        photo: 'assets\/images\/products\/lightsick-mtp-55.jpg',
+    }, {
+        name: 'Mặt trời',
+        price: '45.000',
+        photo: 'assets\/images\/products\/mat-troi-45.jpg',
+    }, {
+        name: 'Mochi donut',
+        price: '35.000',
+        photo: 'assets\/images\/products\/mochi-donut-35.png',
+    }, {
+        name: 'Gà mini',
+        price: '25.000',
+        photo: 'assets\/images\/products\/ga-nho-25.png',
+    }, {
+        name: 'Túi may mắn',
+        price: '30.000',
+        photo: 'assets\/images\/products\/tui-may-man-30.jpg',
+    }, {
+        name: 'Mèo ú',
+        price: '40.000',
+        photo: 'assets\/images\/products\/meo-u-40.png',
+    }, {
+        name: 'Núi & hoa',
+        price: '30.000',
+        photo: 'assets\/images\/products\/nui-hoa-30.png',
+    }, {
+        name: 'Gà mầm cây',
+        price: '50.000',
+        photo: 'assets\/images\/products\/ga-mam-cay-50.png',
+    }, {
+        name: 'Heo cầm dao',
+        price: '55.000',
+        photo: 'assets\/images\/products\/heo-cam-dao-55.jpg',
+    }, {
+        name: 'Stitch',
+        price: '75.000',
+        photo: 'assets\/images\/products\/stitch-75.png',
+    }, {
+        name: 'Vịt tỉu thơ',
+        price: '55.000',
+        photo: 'assets\/images\/products\/vit-tiu-tho-55.png',
+    }, {
+        name: 'Toothlesss',
+        price: '60.000',
+        photo: 'assets\/images\/products\/Toothlesss-60.jpg',
+    }, {
+        name: 'Vịt đội nón mini',
+        price: '45.000',
+        photo: 'assets\/images\/products\/vit-mini-45.png',
+    }, {
+        name: 'Heo nón hoa',
+        price: '55.000',
+        photo: 'assets\/images\/products\/prd-25.png',
+    }, {
+        name: 'Patrick',
+        price: '55.000',
+        photo: 'assets\/images\/products\/patrick-55.jpg',
+    }, {
+        name: 'Vịt nón bèo',
+        price: '60.000',
+        photo: 'assets\/images\/products\/vit-non-beo-60.png',
+    }, {
+        name: 'Vịt nâm xôi',
+        price: '60.000',
+        photo: 'assets\/images\/products\/vit-mam-xoi-60.png',
+    }, {
+        name: 'Tulip',
+        price: '30.000',
+        photo: 'assets\/images\/products\/tulip-mini-30.jpg',
+    }, {
+        name: 'Bạch tuộc tròn',
+        price: '45.000',
+        photo: 'assets\/images\/products\/bach-tuoc-tron-40.png',
+    }, {
+        name: 'Bồ hóng',
+        price: '50.000',
+        photo: 'assets\/images\/products\/bo-hong50.png',
+    }, {
+        name: 'Bánh kem cuộn',
+        price: '28.000',
+        photo: 'assets\/images\/products\/banh-kem-cuon-28.png',
+    }, {
+        name: 'Thỏ Hoa',
+        price: '40.000',
+        photo: 'assets\/images\/products\/tho-hoa40.png',
+    }, {
+        name: 'Calcifer',
+        price: '30.000',
+        photo: 'assets\/images\/products\/calcifer-30.png',
+    }, {
+        name: 'Cà chua',
+        price: '30.000',
+        photo: 'assets\/images\/products\/ca-chua-28.png',
+    }, {
+        name: 'Bò sữa',
+        price: '50.000',
+        photo: 'assets\/images\/products\/bo-sua50.png',
+    }, {
+        name: 'Chó shiba',
+        price: '55.000',
+        photo: 'assets\/images\/products\/cho-shiba-55.png',
+    },
+    {
+        name: 'Mèo dáng ngồi',
+        price: '50.000',
+        photo: 'assets\/images\/products\/meo-ngoi-50.jpg',
+    }, {
+        name: 'Con sứa',
+        price: '55.000',
+        photo: 'assets\/images\/products\/bach-tuoc-xuc-tua-55.jpg',
+    }, {
+        name: 'Đầu loopy',
+        price: '50.000',
+        photo: 'assets\/images\/products\/dau-loopy-50.png',
+    }, {
+        name: 'Mochi bánh giò',
+        price: '60.000',
+        photo: 'assets\/images\/products\/mochi-banh-gio-60.png',
+    }, {
+        name: 'Mèo túi cá',
+        price: '55.000',
+        photo: 'assets\/images\/products\/meo-tui-ca-55.png',
+    }, {
+        name: 'Ong vàng',
+        price: '45.000',
+        photo: 'assets\/images\/products\/ong-45.png',
+    },
+    {
+        name: 'Jake',
+        price: '55.000',
+        photo: 'assets\/images\/products\/jake-55.jpg',
+    }, {
+        name: 'Đầu heo hồng',
+        price: '45.000',
+        photo: 'assets\/images\/products\/dau-heo-hong-45.png',
+    }, {
+        name: 'Minions',
+        price: '45.000',
+        photo: 'assets\/images\/products\/minions-45.png',
+    }, {
+        name: 'Đồ ăn',
+        price: '50.000',
+        photo: 'assets\/images\/products\/do-an-50.png',
+    }, {
+        name: 'Capybara bồn tắm',
+        price: '60.000',
+        photo: 'assets\/images\/products\/capybara-bon-tam-60.png',
+    }, {
+        name: 'Chanh & hoa ',
+        price: '35.000',
+        photo: 'assets\/images\/products\/chanh-35.jpg',
+    }, {
+        name: 'Molang',
+        price: '55.000',
+        photo: 'assets\/images\/products\/molang-55.png',
+    }
+];
